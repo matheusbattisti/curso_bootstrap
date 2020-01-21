a54b3f6ce11d4fa1021e6a8a701a8a3790551498
Matheus Battisti <matheusbudkewicz@gmail.com>
2020-01-21 15:48:17 -0200

    parallax

---
+++ js/scripts.js
@@ -0,0 +1,95 @@
+$( document ).ready(function() {
+
+  // Progress bar
+  let containerA = document.getElementById("circleA");
+
+  let circleA = new ProgressBar.Circle(containerA, {
+
+    color: '#65DAF9',
+    strokeWidth: 8,
+    duration: 1400,
+    from: { color: '#aaa'},
+    to: { color: '#65DAF9'},
+
+    step: function(state, circle) {
+      circle.path.setAttribute('stroke', state.color);
+
+      var value = Math.round(circle.value() * 60);
+      circle.setText(value);
+
+    }
+
+  });
+
+  circleA.animate(1.0);
+
+  let containerB = document.getElementById("circleB");
+
+  let circleB = new ProgressBar.Circle(containerB, {
+
+    color: '#65DAF9',
+    strokeWidth: 8,
+    duration: 1600,
+    from: { color: '#aaa'},
+    to: { color: '#65DAF9'},
+
+    step: function(state, circle) {
+      circle.path.setAttribute('stroke', state.color);
+
+      var value = Math.round(circle.value() * 254);
+      circle.setText(value);
+
+    }
+
+  });
+
+  circleB.animate(1.0);
+
+  let containerC = document.getElementById("circleC");
+
+  let circleC = new ProgressBar.Circle(containerC, {
+
+    color: '#65DAF9',
+    strokeWidth: 8,
+    duration: 1800,
+    from: { color: '#aaa'},
+    to: { color: '#65DAF9'},
+
+    step: function(state, circle) {
+      circle.path.setAttribute('stroke', state.color);
+
+      var value = Math.round(circle.value() * 32);
+      circle.setText(value);
+
+    }
+
+  });
+
+  circleC.animate(1.0);
+
+  let containerD = document.getElementById("circleD");
+
+  let circleD = new ProgressBar.Circle(containerD, {
+
+    color: '#65DAF9',
+    strokeWidth: 8,
+    duration: 2000,
+    from: { color: '#aaa'},
+    to: { color: '#65DAF9'},
+
+    step: function(state, circle) {
+      circle.path.setAttribute('stroke', state.color);
+
+      var value = Math.round(circle.value() * 5423);
+      circle.setText(value);
+
+    }
+
+  });
+
+  circleD.animate(1.0);
+
+  // Parallax
+  $('#data-area').parallax({imageSrc: 'img/cidadeparallax.png'});
+
+});
